a98fa9db7bc4a1774c70c0f40fea175bc19c692d
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-26 21:57:31 -0600

    main controller, welcome method and view, root route, fix wrong proyect name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "proyect_hunt_production"
+  # config.active_job.queue_name_prefix = "product_hunt_production"
 
   config.action_mailer.perform_caching = false
 
