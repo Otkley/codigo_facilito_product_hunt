af5a6fbb4acab5e3ef545578e31719d46a464382
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-26 22:09:32 -0600

    Agregamos un custom layout y lo utilizamos de 2 maneras diferentes, tambien usamos yield

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,12 @@
 class MainController < ApplicationController
+  # Indicamos que este controller utilizara otro layout
+  # layout 'custome'
+
+  # Si queremos que algun metodo en especifico use otro layout
+  # render layout: 'custome'
 
   def welcome
     @name = 'Cody!'
+    render layout: 'custome'
   end
-
 end
