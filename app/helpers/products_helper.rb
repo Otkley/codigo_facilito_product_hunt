e51ac7e6d40d00f76daa724e53b770c348372562
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-27 22:04:39 -0600

    Helper: metodo para usar en vistas ej: render, image_tag, root_path, link_to, etc y propios.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,18 @@
 module ProductsHelper
+  # Este se creo junto con el ProductsController y aqui colocaremos los metodos que queramos utilizar en nuestas vistas.
+
+  def date_format(date)
+    meses = %w[Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre]
+    # obtenemos el mes con %-m y restamos 1 para el match con el arreglo
+    mes = meses[date.strftime('%-m').to_i - 1]
+    dia = date.strftime('%d')
+    year = date.strftime('%Y')
+
+    "#{dia} del #{mes} del #{year}"
+  end
+
+  def simple_description(description)
+    # truncate: devolver # de caracteres especificos
+    description.truncate(100)
+  end
 end
