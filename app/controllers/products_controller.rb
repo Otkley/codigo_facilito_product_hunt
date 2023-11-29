663b58c6ac5b55364370d2d590cf9b242838d1ce
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-28 22:03:09 -0600

    edit,update,before_action, redirect_to, status: :unprocessable, rutas

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,7 @@
 class ProductsController < ApplicationController
+  # metodo que recibe un simbolo a ejecutar antes de las acciones que van dentro del parametro only pasados como simbolo
+  before_action :set_product, only: [:show, :edit, :update]
+
   def index
     @products = Product.where(visible: true).order('id DESC')
   end
@@ -9,7 +12,17 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to product_path(@product), notice: "El producto se actualizo de forma exitosa."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def create
@@ -18,7 +31,7 @@ class ProductsController < ApplicationController
     if @product.persisted?
       # notice es el parametro con el cual nosotros enviamos mensajes a la vista
       # helper_method: redirect_to vista/path, argumento:
-      redirect_to products_path, notice: "El producto se creo de forma exitosa."
+      redirect_to product_path(@product), notice: "El producto se creo de forma exitosa."
     else
       #renderizamos la vista new, y con el status le hacemos saber al navegador que la peticion, no fue procesada
       render :new, status: :unprocessable_entity
@@ -26,6 +39,10 @@ class ProductsController < ApplicationController
   end
 
   private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
 
   def product_params
     params.require(:product).permit(:nombre, :description, :visible)
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'products', to: 'products#index'
   get 'products/new', to: 'products#new', as: 'new_products'
   get 'products/:id', to: 'products#show', as: :product
+  # a partir de los nombres, ruby on rails nos genera los helpers para las rutas
+  get 'products/:id/edit', to: 'products#edit', as: :edit_product
+  patch 'products/:id', to: 'products#update'
   post 'products', to: 'products#create'
 
   root "main#welcome"
