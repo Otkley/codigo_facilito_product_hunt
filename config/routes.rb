b59d09279dc07769e4b42cc061db3fb1920925dc
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-28 21:30:59 -0600

    show action en products controller, ruta, vista y link_to en la card(parcial)

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # get 'products/index'
   get 'products', to: 'products#index'
   get 'products/new', to: 'products#new', as: 'new_products'
+  get 'products/:id', to: 'products#show', as: :product
   post 'products', to: 'products#create'
 
   root "main#welcome"
