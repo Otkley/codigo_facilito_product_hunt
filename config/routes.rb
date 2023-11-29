14a85f435e38e945a1097e23d372831ec32724b8
Sergio Montiel <sergiomontielcastello@gmail.com>
2023-11-28 22:10:31 -0600

    resources :products y explicación

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+=begin
   # get 'products/index'
   get 'products', to: 'products#index'
   get 'products/new', to: 'products#new', as: 'new_products'
@@ -7,6 +8,19 @@ Rails.application.routes.draw do
   get 'products/:id/edit', to: 'products#edit', as: :edit_product
   patch 'products/:id', to: 'products#update'
   post 'products', to: 'products#create'
+
+  un resources creara las rutas para un CRUD completo acorde al controlador
+  Verbos HTTP    Acciones
+  -get            #index
+  -post           #create
+  -get            #new
+  -get            #edit
+  -get            #show
+  -patch          #update
+  -delete         #destroy
+=end
+
+  resources :products
 
   root "main#welcome"
 end
